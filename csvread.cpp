2256c8e000e88753408a0bde930247ebff170b3d
EC3XXX <95997106+EC3XXX@users.noreply.github.com>
2023-11-14 14:57:49 +0800

    Add files via upload

---
+++ csvread.cpp
@@ -0,0 +1,74 @@
+//.csv data file input .cpp
+//by EC3XXX
+//2023/9/13
+//tools for ghame_system_modeling to read .csv data file
+//easy to use examples for model checking
+
+//CANNOT USE WITHOUT csvread.h FILE AND INCLUDE IT
+//DECLARE struct structure_csv csv_csvread AGAIN AT THE TOP OF FILE WHICH ARE USING CSVREAD
+
+//connection with .h file
+#include "csvread.h"
+
+//std namespace
+using namespace std;
+
+//vectors using to record csv contents
+//defined in .h file
+extern struct structure_csv csv_csvread;
+
+//function csvread, use this to read csvfile
+void csvread(string filename)
+{
+	ifstream data(filename);
+	
+	//record the name of columns and store into vector<string> name
+	string firstline;
+	getline(data,firstline);						//read first line only
+	stringstream firstlinestream(firstline);
+	string firstcell;
+	while(getline(firstlinestream,firstcell,','))	//',' means gap
+	{
+		csv_csvread.name.push_back(firstcell);		//store names as string
+	}
+	
+	//record data and store into vector<vector<double>> data
+	//because of pointer moving in previous recording, names aren't included in data vector
+	string line;
+	while(getline(data,line))					//read other lines
+	{
+		stringstream linestream(line);
+		string cell;
+		vector<double> tempdata;
+		while(getline(linestream,cell,','))		//',' means gap
+		{
+			tempdata.push_back(stod(cell));		//std::stod(), transfer string into double
+		}
+		csv_csvread.data.push_back(tempdata);	//store line data into form data
+	}
+	
+	data.close();
+}
+
+//function csvtell, return data in col named name_of_col and row row
+//CAN ONLY USE THIS FUNCTION AFTER USED csvread(filename) ONCE
+//THE ROW NUMBERS START FROM ZERO
+double csvtell(string name_of_col,int row)
+{
+	auto f = find(csv_csvread.name.begin(),csv_csvread.name.end(),name_of_col); //find which col the name is in
+	int col = distance(csv_csvread.name.begin(),f);								//locate the col and row
+	return csv_csvread.data[row][col];											//return
+}
+
+/*
+int main(int argc,char *argv[])
+{
+	void csvread(string filename);
+	double csvtell(string name_of_col,int row);
+	csvread("test.csv");
+	cout << csv.data[3][3] << endl;
+	cout << csvtell("psisbx",3);
+	
+	
+	return 0;
+}*/
---
+++ csvread.h
@@ -0,0 +1,34 @@
+//.csv data file input .h
+//by EC3XXX
+//2023/9/13
+//tools for ghame_system_modeling to read .csv data file
+//easy to use examples for model checking
+
+//CANNOT USE WITHOUT csvread.cpp FILE
+
+//avoid redeclaration
+#ifndef CSVREAD_H
+#define CSVREAD_H
+
+//include files
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+//std namespace
+using namespace std;
+
+//defination of structrue_csv, using in csvread.cpp and the file where need csvread functions
+struct structure_csv{
+	vector<string> name;			//name storage, strings
+	vector<vector<double>> data;	//data storage, double, 2d
+};
+
+void csvread(string filename);				//function csvread, use this to read csvfile
+double csvtell(string name_of_col,int row);	//function csvtell, return data in col named name_of_col and row row
+											//CAN ONLY USE THIS FUNCTION AFTER USED csvread(filename) ONCE
+											//THE ROW NUMBERS START FROM ZERO
+
+#endif
